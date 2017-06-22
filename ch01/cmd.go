3b3edd858364c3a3527b5ac6c679b7c0ffd6b310
NickNYU <cz739@nyu.edu>
2017-06-22 22:21:57 +0800

    Complete first chapter

---
+++ ch01/cmd.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	class       string
+	args        []string
+}
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+
+	flag.Usage = printUsage
+	// Set up helpFlag
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	// Set up versionFlag
+	flag.BoolVar(&cmd.versionFlag, "version", false, "version")
+	// Set up cpOption
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+
+	return cmd
+
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
