e7deda52f235cfc2cc6f749929dd5745878b88d6
Nick Zhu <Nick.Zhu@emc.com>
2017-06-28 18:12:27 +0800

    chapter-3 copy author's code, and test passed

---
+++ ch03/classfile/attr_markers.go
@@ -0,0 +1,29 @@
+package classfile
+
+/*
+Deprecated_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+}
+*/
+type MarkerAttribute struct{}
+
+type DeprecatedAttribute struct {
+	MarkerAttribute
+}
+
+/*
+Synthetic_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+}
+*/
+type SyntheticAttribute struct {
+	MarkerAttribute
+}
+
+
+
+func (self *MarkerAttribute) readInfo(reader *ClassReader) {
+	// read nothing
+}
