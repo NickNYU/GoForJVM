300d342e3b7f829763d01572ad09d319163fc8a5
NickNYU <cz739@nyu.edu>
2017-07-08 22:53:06 +0800

    Chapter-5

---
+++ ch05/instructions/math/rem.go
@@ -0,0 +1,31 @@
+package math
+
+import (
+	"jvmgo/ch05/instructions/base"
+	"jvmgo/ch05/rtda"
+	"math"
+)
+
+type DREM struct { base.NoOperandsInstruction }
+type FREM struct { base.NoOperandsInstruction }
+type IREM struct { base.NoOperandsInstruction }
+type LREM struct { base.NoOperandsInstruction }
+
+func (self *IREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopInt()
+	val1 := stack.PopInt()
+	if val2 == 0 {
+		panic("java.lang.ArithmeticException: /   by zero")
+	}
+	result := val1 % val2
+	stack.PushInt(result)
+}
+
+func (self *DREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val2 := stack.PopDouble()
+	val1 := stack.PopDouble()
+	result := math.Mod(val1, val2)
+	stack.PushDouble(result)
+}
