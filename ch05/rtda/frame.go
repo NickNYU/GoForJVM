935e4fc4dc586e551d839d261dac956aed43a01e
NickNYU <cz739@nyu.edu>
2017-07-16 23:32:37 +0800

    Mark points on chapter-5

--- ch05/rtda/frame.go
+++ ch05/rtda/frame.go
@@ -8,6 +8,7 @@ type Frame struct {
 	nextPC       int // the next instruction after the call
 }
 
+// Mark: JVM在编译阶段就完成了对 1. 局部变量表，2. 操作栈 大小的确定，在运行时，栈帧大小是完全确定的
 func newFrame(thread *Thread, maxLocals, maxStack uint) *Frame {
 	return &Frame{
 		thread:       thread,
